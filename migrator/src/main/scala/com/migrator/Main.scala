5c7ede5cff2d5826504f1a63a8f15d583c758198
david <dvdprr6@gmail.com>
2024-04-10 08:08:09 -0400

    cleanup

--- migrator/src/main/scala/com/migrator/Main.scala
+++ migrator/src/main/scala/com/migrator/Main.scala
@@ -4,6 +4,7 @@ import com.migrator.dto.CustomerDto
 import com.migrator.models.MigratorOptions
 import com.migrator.repository.CustomerRepository
 import com.migrator.service.{MongoDbMigratorService, PostgresMigratorService}
+import com.migrator.utils.MongoDbConnection
 import zio._
 import zio.cli.HelpDoc.Span.text
 import zio.cli.{CliApp, Command, Options, ZIOCliDefault}
@@ -12,58 +13,6 @@ import java.time.LocalDate
 import java.util.UUID
 
 object Main extends ZIOCliDefault {
-
-//  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
-//
-//    val options: Options[Boolean] = Options.boolean("local").alias("l")
-//    val postgresUrl: Options[String] = Options.text("postgres-url")
-//    val postgresUsername: Options[String] = Options.text("postgres-username")
-//    val postgresPassword: Options[String] = Options.text("postgres-password")
-//    val mongoDbUrl: Options[String] = Options.text("mongodb-url")
-//
-//    val arguments: Args[String] = Args.text("repository")
-//    val postgresUrlArgs: Args[String] = Args.text("postgres url")
-//    val help: HelpDoc = HelpDoc.p("Creates a copy of an existing repository")
-//
-//    val command: Command[(Boolean, String)] = Command("clone").subcommands(
-//      Command("clone", options, arguments).withHelp(help),
-//      Command("postgres url", postgresUrl, postgresUrlArgs))
-//
-//    // Define val cliApp using CliApp.make
-//    val cliApp = CliApp.make(
-//      name = "Sample Git",
-//      version = "1.1.0",
-//      summary = text("Sample implementation of git clone"),
-//      command = command
-//    ) {
-//      // Implement logic of CliApp
-//      case _ => printLine("executing git clone")
-//    }
-//
-//
-//    val customerDto = Seq(
-//      CustomerDto(
-//        id = UUID.randomUUID(),
-//        age = 35,
-//        dob = LocalDate.of(1988, 6, 8),
-//        firstName = "David",
-//        lastName = "Parr"
-//      )
-//    )
-//
-//    val program = for{
-//      mongoDbMigratorService <- ZIO.service[MongoDbMigratorService]
-//      postgresMigratorService <- ZIO.service[PostgresMigratorService]
-//      _ <- postgresMigratorService.performPostgresMigration(customerDto)
-//    } yield ()
-//
-//    program.provide(
-//      MongoDbConnection.live,
-//      MongoDbMigratorService.live,
-//      CustomerRepository.live,
-//      PostgresMigratorService.live
-//    )
-//  }
 
   override def cliApp: CliApp[Any with ZIOAppArgs with Scope, Any, Any] = {
     val postgresUrl: Options[String] = Options.text("postgres-url")
@@ -97,11 +46,14 @@ object Main extends ZIOCliDefault {
     )
 
     val program = for {
+      mongoDbMigratorService <- ZIO.service[MongoDbMigratorService]
       postgresMigratorService <- ZIO.service[PostgresMigratorService]
       _ <- postgresMigratorService.performPostgresMigration(customerDto, postgresUrl, postgresUsername, postgresPassword)
     } yield()
 
     program.provide(
+      MongoDbConnection.live,
+      MongoDbMigratorService.live,
       PostgresMigratorService.live,
       CustomerRepository.live
     )
