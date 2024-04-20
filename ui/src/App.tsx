07814d5a694b205e1c88f70e0e9577fac4f4538e
david <dvdprr6@gmail.com>
2024-04-20 11:59:58 -0400

    add app bar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,34 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createTheme, Theme } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {Navigation} from "./components/navigation";
+import CssBaseline from '@mui/material/CssBaseline';
 
-function App() {
-  const [count, setCount] = useState(0)
+const theme: Theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1DB954'
+    },
+    secondary: {
+      main: '#1DB954'
+    },
+    background: {
+      default: '#303030',
+      paper: '#424242'
+    },
+  },
+  typography: {
+    button: {
+      textTransform: 'none'
+    }
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          background: '#1d5bb9'
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        text: {
+          size: '10',
+          textTransform: 'lowercase'
+        }
+      }
+    },
+    MuiRadio: {
+      styleOverrides: {
+        root:{
+          color: '#1DB954',
+          '&.Mui-checked': {
+            color: '#1DB954'
+          }
+        }
+      }
+    },
+    MuiCircularProgress: {
+      styleOverrides: {
+        root:{
+          size: 20,
+          color:'#1DB954'
+        }
+      }
+    }
+  }
+})
 
+const router = createBrowserRouter([
+  {
+    path: '/BudgetTracker',
+    element: <Navigation />
+  }
+])
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
 
