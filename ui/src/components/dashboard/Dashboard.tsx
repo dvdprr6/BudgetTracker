49b4c2ff5ed4e07583a9202ccc85e35962c5549c
david <dvdprr6@gmail.com>
2024-04-21 15:16:40 -0400

    dashboard template

--- ui/src/components/dashboard/Dashboard.tsx
+++ ui/src/components/dashboard/Dashboard.tsx
@@ -1,7 +1,180 @@
+import { useState, ChangeEvent } from 'react'
+import { PieChart } from '@mui/x-charts'
+import {
+  Grid, Box, Card, Paper, Table,
+  TableBody, TableCell, TableContainer,
+  TableHead, TablePagination, TableRow
+} from '@mui/material'
 
+interface Column {
+  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toFixed(2),
+  },
+];
+
+interface Data {
+  name: string;
+  code: string;
+  population: number;
+  size: number;
+  density: number;
+}
+
+function createData(
+  name: string,
+  code: string,
+  population: number,
+  size: number,
+): Data {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('Expense', 'IN', 1324171354, 3287263),
+  createData('Revenue', 'CN', 1403500365, 9596961),
+  // createData('Italy', 'IT', 60483973, 301340),
+  // createData('United States', 'US', 327167434, 9833520),
+  // createData('Canada', 'CA', 37602103, 9984670),
+  // createData('Australia', 'AU', 25475400, 7692024),
+  // createData('Germany', 'DE', 83019200, 357578),
+  // createData('Ireland', 'IE', 4857000, 70273),
+  // createData('Mexico', 'MX', 126577691, 1972550),
+  // createData('Japan', 'JP', 126317000, 377973),
+  // createData('France', 'FR', 67022000, 640679)
+];
 
 const Dashboard = () => {
-  return <p>This is dashboard</p>
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Card variant={'outlined'} sx={{ height: '100%'}}>
+          <Box alignItems={'center'} display={'flex'} p={2}>
+            <PieChart
+              series={[
+                {
+                  data: rows.map(((item, index) => ({ id: index, value: item.population, label: item.name }))),
+                },
+              ]}
+              width={600}
+              height={400}
+              // slotProps={{
+              //   legend: { position: 'horizontal' }
+              // }}
+            />
+          </Box>
+        </Card>
+      </Grid>
+      <Grid item xs={6}>
+        <Card variant={'outlined'} sx={{ height: '100%'}}>
+          <Box alignItems={'center'} display={'flex'} p={2}>
+            This Box uses MUI System props for quick customization.
+          </Box>
+          <Box alignItems={'center'} display={'flex'} p={2}>
+            This Box uses MUI System props for quick customization.
+          </Box>
+          <Box alignItems={'center'} display={'flex'} p={2}>
+            This Box uses MUI System props for quick customization.
+          </Box>
+          <Box alignItems={'center'} display={'flex'} p={2}>
+            This Box uses MUI System props for quick customization.
+          </Box>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card variant={'outlined'} sx={{ height: '100%'}}>
+          <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      return (
+                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                          {columns.map((column) => {
+                            const value = row[column.id];
+                            return (
+                              <TableCell key={column.id} align={column.align}>
+                                {column.format && typeof value === 'number'
+                                  ? column.format(value)
+                                  : value}
+                              </TableCell>
+                            );
+                          })}
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </Card>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Dashboard
