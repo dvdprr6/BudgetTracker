9b4387ea2b996f0f7b4f62403554e059515bd841
david <dvdprr6@gmail.com>
2025-01-12 13:17:29 -0500

    realign card content in pie chard

--- ui/src/components/common/BudgetTrackerPieChart.tsx
+++ ui/src/components/common/BudgetTrackerPieChart.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { PieChart } from '@mui/x-charts'
-import { Box, Card } from '@mui/material'
+import { Box, Card, CardContent } from '@mui/material'
 
 type TBudgetTrackerPieChart = {
   data: { id: string, value: number, label: string }[],
@@ -13,20 +13,22 @@ const BudgetTrackerPieChart: FC<TBudgetTrackerPieChart> = (props) => {
 
   return (
     <Card variant={'outlined'} sx={{ height: '100%'}}>
-      <Box alignItems={'center'} display={'flex'} p={2}>
-        <PieChart
-          series={[
-            {
-              data: data,
-              highlightScope: { faded: 'global', highlighted: 'item' },
-              faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-              valueFormatter: v => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(v.value)
-            }
-          ]}
-          width={width}
-          height={height}
-        />
-      </Box>
+      <CardContent>
+        <Box alignItems={'center'} display={'flex'}>
+          <PieChart
+            series={[
+              {
+                data: data,
+                highlightScope: { faded: 'global', highlighted: 'item' },
+                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                valueFormatter: v => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(v.value)
+              }
+            ]}
+            width={width}
+            height={height}
+          />
+        </Box>
+      </CardContent>
     </Card>
   )
 }
